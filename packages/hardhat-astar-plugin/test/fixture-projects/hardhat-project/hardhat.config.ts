037d6d60527aafd00d62cc0b86adc25b6f09504f
jim4067 <jimmyimpulse2@gmail.com>
2021-12-11 22:56:26 +0300

    Addres changes requested * change to yarn and delete package-lock.json * remove .editor-config * create .mocharc.json for the tests * write a simple test to see whether CI passes

--- packages/hardhat-astar-plugin/test/fixture-projects/hardhat-project/hardhat.config.ts
+++ packages/hardhat-astar-plugin/test/fixture-projects/hardhat-project/hardhat.config.ts
@@ -1,14 +1,10 @@
 // We load the plugin here.
-import { HardhatUserConfig } from "hardhat/types";
+import { HardhatUserConfig } from 'hardhat/types';
 
-import "../../../src/index";
+import '../../../src/index';
 
 const config: HardhatUserConfig = {
-  solidity: "0.7.3",
-  defaultNetwork: "hardhat",
-  paths: {
-    newPath: "asd",
-  },
+  solidity: '0.7.3'
 };
 
 export default config;
