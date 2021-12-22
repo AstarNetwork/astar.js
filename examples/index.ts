fb07dfd3230bc848761b9162ed65db19646706f8
sushi30 <imri.paran@gmail.com>
2021-12-22 19:23:13 +0000

    added exit

--- examples/index.ts
+++ examples/index.ts
@@ -2,7 +2,6 @@ import { ApiPromise } from '@polkadot/api';
 import { WsProvider } from '@polkadot/rpc-provider';
 import { options } from '@astar-network/astar-api';
 
-
 async function main() {
   const provider = new WsProvider('wss://shiden.api.onfinality.io/public-ws');
   const api = new ApiPromise(options({ provider }));
@@ -10,4 +9,4 @@ async function main() {
   console.log({ api });
 }
 
-main();
+main().then(() => process.exit());
