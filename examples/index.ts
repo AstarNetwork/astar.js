362d96a4a0feaaf42927759af98a46ec87792cdb
sushi30 <imri.paran@gmail.com>
2021-12-22 19:08:03 +0000

    Working example

---
+++ examples/index.ts
@@ -0,0 +1,13 @@
+import { ApiPromise } from '@polkadot/api';
+import { WsProvider } from '@polkadot/rpc-provider';
+import { options } from '@astar-network/api';
+
+
+async function main() {
+  const provider = new WsProvider('wss://localhost:9944');
+  const api = new ApiPromise(options({ provider }));
+  await api.isReady;
+  console.log({ api });
+}
+
+main();
