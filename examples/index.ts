b76a8d2493ab2db954e96861ab64a61719ac77c8
sushi30 <imri.paran@gmail.com>
2021-12-22 19:14:56 +0000

    Use "astar-api" instead of "api"

--- examples/index.ts
+++ examples/index.ts
@@ -1,6 +1,6 @@
 import { ApiPromise } from '@polkadot/api';
 import { WsProvider } from '@polkadot/rpc-provider';
-import { options } from '@astar-network/api';
+import { options } from '@astar-network/astar-api';
 
 
 async function main() {
