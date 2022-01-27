a11fc548c72feca08535bd5f4f231a35b8208786
Drew Stone <drewstone329@gmail.com>
2022-01-27 11:57:11 -0500

    Connect to Astar

--- examples/index.ts
+++ examples/index.ts
@@ -2,11 +2,23 @@ import { ApiPromise } from '@polkadot/api';
 import { WsProvider } from '@polkadot/rpc-provider';
 import { options } from '@astar-network/astar-api';
 
-async function main() {
+async function connectToShiden() {
   const provider = new WsProvider('wss://shiden.api.onfinality.io/public-ws');
   const api = new ApiPromise(options({ provider }));
   await api.isReady;
-  console.log({ api });
+  console.log((await api.rpc.system.properties()).toHuman());
 }
 
-main().then(() => process.exit());
+async function connectToAstar() {
+  const provider = new WsProvider('wss://astar.api.onfinality.io/public-ws');
+  const api = new ApiPromise(options({ provider }));
+  await api.isReady;
+  console.log((await api.rpc.system.properties()).toHuman());
+}
+
+(async () => {
+  await connectToShiden();
+  await connectToAstar();
+  process.exit(0);
+})();
+
