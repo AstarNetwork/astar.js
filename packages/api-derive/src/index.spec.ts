ca82daed14be6ed5bb2144b9553432510986e4d3
Nikhil Ranjan <niklabh811@gmail.com>
2022-09-01 15:54:01 +0200

    changed dappStaking address

--- packages/api-derive/src/index.spec.ts
+++ packages/api-derive/src/index.spec.ts
@@ -2,7 +2,7 @@ import { ApiPromise, WsProvider } from '@polkadot/api';
 import { options } from '@astar-network/astar-api';
 
 const getAddressEnum = (address: string) => ({ Evm: address });
-const address = '0x072416b9df2382a62Df34956DffB7B0aDdf668F9';
+const address = '0xf87c7872eff6f01de8efcb328471967b19e302a9';
 
 const endpoint = 'wss://shiden.api.onfinality.io/public-ws';
 // const endpoint = 'ws://localhost:9944';
