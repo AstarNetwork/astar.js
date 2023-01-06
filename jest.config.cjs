886a9e094ce461912bb3b840b0332844cb2ffb02
Nikhil Ranjan <niklabh811@gmail.com>
2023-01-06 11:12:52 +0000

    Fix sdk-core build (#28)
    * Fix sdk-core build
    
    * removed test build
    
    Co-authored-by: Github Actions <action@github.com>

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
     '@astar-network/astar-api-derive(.*)$': '<rootDir>/packages/api-derive/src/$1',
     '@astar-network/astar-api(.*)$': '<rootDir>/packages/api/src/$1',
     '@astar-network/astar-types(.*)$': '<rootDir>/packages/types/src/$1',
-    '@astar-network/astar-type-definitions(.*)$': '<rootDir>/packages/type-definitions/src/$1'
+    '@astar-network/astar-type-definitions(.*)$': '<rootDir>/packages/type-definitions/src/$1',
+    '@astar-network/astar-sdk-core(.*)$': '<rootDir>/packages/sdk-core/src/$1'
   },
   testTimeout: 30000
 };
--- packages/sdk-core/src/index.ts
+++ packages/sdk-core/src/index.ts
@@ -4,3 +4,5 @@ export * from './formatUnitAmount';
 export * from './parseTo18Decimals';
 export * from './reduceBalanceToDenom';
 export * from './units';
+
+export const sdk = {};
