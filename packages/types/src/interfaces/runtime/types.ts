645f89242a395fb644ca6a44f5cfa72f1189ff05
Nikhil Ranjan <niklabh811@gmail.com>
2023-02-10 11:50:19 +0000

    API bump v0.1.14 (#32)
    * API update 9.12.1
    
    * fix
    
    * removed stakers
    
    * removed stakers
    
    * fix
    
    * Package upgrade
    
    * fix weight weight2
    
    * Weight fix
    
    * test
    
    ---------
    
    Co-authored-by: Github Actions <action@github.com>

--- packages/types/src/interfaces/runtime/types.ts
+++ packages/types/src/interfaces/runtime/types.ts
@@ -426,10 +426,13 @@ export interface ValidatorIdOf extends ValidatorId {}
 export interface Wasm extends AccountId {}
 
 /** @name Weight */
-export interface Weight extends WeightV1 {}
+export interface Weight extends WeightV2 {}
 
 /** @name WeightMultiplier */
 export interface WeightMultiplier extends Fixed64 {}
+
+/** @name WeightV0 */
+export interface WeightV0 extends u32 {}
 
 /** @name WeightV1 */
 export interface WeightV1 extends u64 {}
