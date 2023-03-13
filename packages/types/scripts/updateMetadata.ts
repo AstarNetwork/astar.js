df8eeeabcbd2351487dfcace94d1115c00b0b25b
Nikhil Ranjan <niklabh811@gmail.com>
2023-03-13 10:36:48 +0100

    metadata

--- packages/types/scripts/updateMetadata.ts
+++ packages/types/scripts/updateMetadata.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access,
 @typescript-eslint/no-unsafe-argument, @typescript-eslint/restrict-template-expressions */
+import fs from 'fs';
 import { w3cwebsocket as WebSocket } from 'websocket';
 
 const main = (): void => {
@@ -9,9 +10,8 @@ const main = (): void => {
     ws.send('{"id":"1","jsonrpc":"2.0","method":"state_getMetadata","params":[]}');
   };
   ws.onmessage = (msg: any): void => {
-    // const metadata = JSON.parse(msg.data).result;
-    // fs.writeFileSync('packages/types/src/metadata/static-latest.ts', `export default '${metadata}'`);
-    console.log(msg.data);
+    const metadata = JSON.parse(msg.data).result;
+    fs.writeFileSync('packages/types/src/metadata/static-latest.ts', `export default '${metadata}'`);
     process.exit(0);
   };
 };
